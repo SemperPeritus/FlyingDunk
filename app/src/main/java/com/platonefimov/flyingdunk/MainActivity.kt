d371f4b7827887c3df3dc005c053f616bda41fc3
Platon Efimov <platon.efimov@gmail.com>
2017-12-01 01:22:24 +0700

    Blank fullscreen

--- app/src/main/java/com/platonefimov/flyingdunk/MainActivity.kt
+++ app/src/main/java/com/platonefimov/flyingdunk/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.platonefimov.flyingdunk
 
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Fullscreen parameters
+        var uiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                View.SYSTEM_UI_FLAG_FULLSCREEN
+        if (Build.VERSION.SDK_INT >= 19)
+            uiVisibility = uiVisibility or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+
+        // Go Fullscreen
+        window.decorView.systemUiVisibility = uiVisibility
+        actionBar?.hide()
     }
 }
